5995cb2844bf15dd1ad0209fb70098849f7d7990
Abhishek <ap8335235private@gmail.com>
2020-09-29 13:49:40 +0530

    tiktok server commit

---
+++ data.js
@@ -0,0 +1,37 @@
+const data= [
+    {
+        url: "https://firebasestorage.googleapis.com/v0/b/tiktok-clone-25452.appspot.com/o/tiktokvideo_1601031106.mp4?alt=media&token=124c1e2e-e795-41f4-82fb-aa25cd4ed239",
+        channel:"abhi8335",
+        song:"man who sold the world",
+        likes: "37921",
+        messages:"3121",
+        description: "This is an example description",
+        shares: "500"
+    }
+    ,
+    {
+        url: "https://firebasestorage.googleapis.com/v0/b/tiktok-clone-25452.appspot.com/o/tiktokvideo_1601031002.mp4?alt=media&token=c137c4e1-7f56-4b48-9a0d-60b2722e1c96",
+        channel:"abhi8335",
+        song:"man who sold the world",
+        likes: "37921",
+        messages:"3121",
+        description: "This is an example description",
+        shares: "500"
+    }
+]
+
+
+
+export default data;
+
+/*
+{
+        "url": "https://firebasestorage.googleapis.com/v0/b/tiktok-clone-25452.appspot.com/o/tiktokvideo_1601031106.mp4?alt=media&token=124c1e2e-e795-41f4-82fb-aa25cd4ed239",
+        "channel":"abhi8335",
+        "song":"man who sold the world",
+        "likes": "37921",
+        "messages":"3121",
+        "description": "This is an example description",
+        "shares": "500"
+    }
+*/
---
+++ server.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import Data from './data.js'
+import Videos from './dbModel.js'
+
+
+
+//app config
+const app= express();
+const port= 9000;
+
+//middlewares
+app.use(express.json());
+app.use((req,res,next) => {
+    res.setHeaders("Access-Control-Allow-Origin","*"),
+    res.setHeaders("Access-Control-Allow-Headers","*"),
+    next();
+});
+
+
+//db config
+const connection_url= "mongodb+srv://abhi:abhi@tiktok@cluster0.j4lfi.mongodb.net/tiktok?retryWrites=true&w=majority";
+mongoose.connect(connection_url,{
+   useNewUrlParser: true,
+   useCreateIndex: true,
+   useUnifiedTopology: true   
+}).then(res => console.log('DATABASE CONNECTED'));
+
+//api endpoints
+
+app.get('/',(req,res)=> {
+    res.status(200).send('hello world');
+});
+
+app.get('/v1/posts',(req,res) => {
+    res.status(200).send(Data);
+});
+
+app.get('/v2/posts',(req,res) => {
+    Videos.find((err,data) => {
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(200).send(data);
+        }
+    });
+});
+
+app.post('/v2/posts',(req,res) => {
+    //POST request is to ADD DATA to the database
+    const dbVideos= req.body;
+    Videos.create(dbVideos, (err,data) => {
+        if(err){
+            res.status(500).send(err);
+        }else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+
+//listen
+app.listen(port,()=>{
+    console.log(`server is running on ${port}`);
+});
+
+ 
+
+
+
+ 
